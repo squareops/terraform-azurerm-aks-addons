7a3f2e6ac5534903cb35a401e8dfe8b930969a65
Anoushka Akhoury <anoushka@squareops.com>
2023-06-22 15:10:41 +0530

    Update azure disk resource in main.tf of module

--- addons/azure-disk-storage-class/main.tf
+++ addons/azure-disk-storage-class/main.tf
@@ -3,19 +3,19 @@ resource "kubernetes_storage_class_v1" "single_az_sc" {
   metadata {
     name = var.single_az_storage_class_name
   }
+  parameters = {
+    storageaccounttype = "StandardSSD_ZRS"
+  }
+
   storage_provisioner    = "kubernetes.io/azure-disk"
   reclaim_policy         = "Retain"
   allow_volume_expansion = true
   volume_binding_mode    = "WaitForFirstConsumer"
-  parameters = {
-    skuName       = "StandardSSD_ZRS"
-  }
+
   allowed_topologies {
     match_label_expressions {
-      key = "failure-domain.beta.kubernetes.io/zone"
-      values = [
-        var.zone
-      ]
+      key = "topology.kubernetes.io/zone"
+      values = ["${var.zone}"]
     }
   }
 }
